3a2b5e21cbb91d5819c51af8e9f41754e864317f
jyotir <jyotiraditya@falabella.cl>
2022-02-23 12:43:40 +0530

    unit test two

--- src/controllers/courseController.test.js
+++ src/controllers/courseController.test.js
@@ -1,14 +1,16 @@
 const Schema = require('../models/course')
 
-// const create = require('./courseController');
+const getCourse = require('./courseController');
 
 describe('creating',() =>{
     it.only('',async()=>{
         Schema.find = jest.fn().mockReturnValueOnce({
             name: "Jyo",
         })
-   
-    // Schema.prototype.save = jest.fn().mockImplementation(()=>{});
-    // await expect(create("Jyotir","Sinha", "Intern","2015")).rejects.toThrowError();
-})
+    // it.create()
+    })
+    it.only('',async() => {
+        getCourse.getCourse = jest.fn().mockReturnValueOnce()
+
+    })
 })
